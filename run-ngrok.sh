bc43cca90c17b37c0364bf1664caacc817289ed5
HexLLC <hexxmailer@gmail.com>
2025-01-14 16:38:17 +0530

    Delete run-ngrok.sh

--- run-ngrok.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start SSH service in the background
-/usr/sbin/sshd -D &
-
-# Run ngrok HTTP tunnel on port 80 using the provided authtoken
-ngrok http 80 --authtoken ${NGROK_AUTHTOKEN}
